3ac61c979e123531cf12f9a49260d7b1c050b105
Ryan Kelly <ryan@rfk.id.au>
2017-11-15 17:00:57 +1100

    feat(tests): Add test vectors generated via python script.

--- test/relier/KeyUtils.js
+++ test/relier/KeyUtils.js
@@ -53,6 +53,31 @@ describe('KeyUtils', function () {
         });
 
     });
+
+    it('can decrypt a test vector key bundle generated via python code', () => {
+      const keyUtils = new window.fxaCryptoRelier.KeyUtils();
+      keyUtils.keystore = jose.JWK.createKeyStore();
+      return keyUtils.keystore.add({
+        "kty": "EC",
+        "crv": "P-256",
+        "d":   "KXAjjEr4KT9UlYI4BE0BefVdoxP8vqO389U7lQlCigs",
+        "x":   "SiBn6uebjigmQqw4TpNzs3AUyCae1_sG2b9Fzhq3Fyo",
+        "y":   "q99Xq1RWNTFpk99pdQOSjUvwELss51PkmAGCXhLfMV4"
+      })
+      .then(() => {
+        return keyUtils.decryptBundle('eyJhbGciOiJFQ0RILUVTIiwiZW5jIjoiQTI1NkdDTSIsImVwayI6eyJjcnYiOiJQLTI1NiIsImt0eSI6IkVDIiwieCI6Ik40elBSYXpCODd2cGVCZ0h6RnZrdmRfNDhvd0ZZWXhFVlhSTXJPVTZMRG8iLCJ5IjoiNG5jVXhONnhfeFQxVDFrenlfU19WMmZZWjd1VUpUX0hWUk5aQkxKUnN4VSJ9fQ.._0sYf7HdWuRv2cM0.U5ZK5BYZWhLluS7q4y4ZFW1UwcW-s7mjuY_khe4OVjtvLE5jOQw-qGyT_06wY2zpqN6FhGMa16Qhn4UABz0LuDwAfrHOtfRlpqeV3nrKhas2gXt1yLvDFLide4hEPfBJk60t2CXjxprsA1BulinIER2EIJbA.rpf5rzO78Hj-9CWRTLx7TQ')
+      })
+      .then((result) => {
+        assert.deepEqual(result, {
+          "app_key": {
+            "k": "rTcZ5olrrJWqVD6bVtLjHJT0P6d_9IdpEgWT4zVzMb0",
+            "kid": "1510726317-UvyHCg_RD3zKl_hQdlRsfw",
+            "kty": "oct"
+           }
+        })
+      });
+
+    });
   });
 
 });
